93cd5a3a577b416e80d3aa2062ad8374ff8edef9
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-05-30 14:27:35 +0545

    Added web-tech lab exercise

--- 4. Login Form/login.php
+++ 4. Login Form/login.php
@@ -5,27 +5,18 @@
 </head>
 
 <body>
-    <form action="loginprocess.php" method="post">
+    <form action="" method="post">
         <fieldset>
             <legend>Login</legend>
-            <div>
-                <?php
-                if(isset($_GET['error message'])){
-                    echo $_GET['error-message'];
-                }
-                ?>
-            </div>
-            <div>
-                <label for="username">Username</label>
-                <input value="<?php echo isset($_GET['username'])? $_GET['username']: '' ; ?>" type="text" name="username" id="username">
-            </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password">
-            </div>
-            <div>
-                <button type="submit">Login</button>
-            </div>
+            
+            <label for="username">Username</label><br>
+            <input type="text" name="username" id="username"><br><br>
+            
+            <label for="password">Password</label><br>
+            <input type="password" name="password" id="password"><br><br>
+            
+            <button type="submit">Login</button>
+            
         </fieldset>
     </form>
 </body>
